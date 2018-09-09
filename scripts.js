9864afb4960af22fb8c50dcfb4278357e23d6d59
Ryan Navarro <ryannavarro@ymail.com>
2018-09-09 16:58:01 -0700

    Completed slide out menu
    completed slide out menu functionality.
    fixing midtitle responsiveness

--- scripts.js
+++ scripts.js
@@ -6,20 +6,23 @@ const closeBtn = document.querySelector(".close");
 const menuList = document.querySelectorAll(".slideitem");
 const slideOutList = document.querySelector("#slideoutlist");
 const container = document.querySelector(".home");
+const body = document.querySelector(".home");
 
+
+//Sliding menu
 function menuSlide(){
-
    menuOverlay.classList.toggle("openmenu");
-
-     if(menuOverlay.classList.contains("openmenu")){
-
+     if(!menuOverlay.classList.contains("openmenu")){
        slideOutList.classList.add("fadeinlist");
-
      }else{
-
        slideOutList.classList.remove("fadeinlist");
+     };
+};
 
-     }
+body.onclick = function(event){
+  if(!event.target.matches(".menuoverlay") && !event.target.matches(".menuicon")){
+    menuOverlay.classList.remove("openmenu");
+  }
 };
 
 menuBtn.addEventListener("click", menuSlide);
